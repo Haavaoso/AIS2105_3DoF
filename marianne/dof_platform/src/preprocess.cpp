56c0d1183c0701c8a9b5d35ee78ceef968944ad3
Håvard Olai Solbakken <123083569+Haavaoso@users.noreply.github.com>
2024-05-13 16:25:22 +0200

    Update preprocess.cpp
    changed kernel to 9x9
    change sigma with ros2 param set /image_processor sigma x   
    x as the value

--- marianne/dof_platform/src/preprocess.cpp
+++ marianne/dof_platform/src/preprocess.cpp
@@ -6,6 +6,8 @@
 #include <image_transport/image_transport.hpp>
 #include <opencv2/opencv.hpp>
 #include <memory>
+#include <iostream>
+
 
 class ImageProcessor : public rclcpp::Node
 {
@@ -15,23 +17,35 @@ public:
     subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
       "image_raw", 10, std::bind(&ImageProcessor::topic_callback, this, std::placeholders::_1));
 
-    publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image_clone", 10);
-    cv::namedWindow("Raw image for cam");
+    publisher_ = this->create_publisher<sensor_msgs::msg::Image>("pre_processed_image", 10);
+    this->declare_parameter<int>("sigma", 3);
+
   }
 
 private:
   void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg)
   {
+    int sigma = this->get_parameter("sigma").as_int();
+
+
+
     cv_bridge::CvImagePtr cv_raw_image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     
     //klon bilde 
-    cv::Mat image_clone = cv_raw_image->image.clone();
+    cv::Mat working_img = cv_raw_image->image.clone();
 
+    //Koverter til grå
+    cv::Mat img_gray;
+    cv::cvtColor(working_img, img_gray, cv::COLOR_BGR2GRAY);
+
+    cv::Mat img_blur;
+    cv::GaussianBlur(img_gray, img_blur, cv::Size(9,9), sigma, sigma);
+    
+  
     //Sender prossesert bilde 
-    auto cloned_image_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image_clone).toImageMsg();
-    publisher_->publish(*cloned_image_msg);
+    auto pre_processed_image = cv_bridge::CvImage(std_msgs::msg::Header(), "mono8", img_blur).toImageMsg(); 
+    publisher_->publish(*pre_processed_image);
   }
-
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
 };
